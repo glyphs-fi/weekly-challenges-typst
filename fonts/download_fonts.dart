3a593df0230b1971e028d8083ed53247aa5a4b01
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-02-19 23:24:22 +0100

    Started writing script to download all required fonts

---
+++ fonts/download_fonts.dart
@@ -0,0 +1,69 @@
+#!/usr/bin/env dart
+
+import "dart:io";
+import "dart:typed_data";
+
+import "package:http/http.dart" as http;
+import "package:path/path.dart" as p;
+
+final List<String> googleFontsLinks = [
+  "https://fonts.googleapis.com/css2?family=STIX+Two+Text:ital,wght@0,400..700;1,400..700",
+  "https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400..900",
+];
+
+typedef FontFileDefinition = ({String family, String style, String weight, String url});
+
+final http.Client client = http.Client();
+
+Future<void> main(List<String> args) async {
+  final Directory fontsDir = Directory(p.dirname(Platform.script.toFilePath()));
+  final Set<FontFileDefinition> fontFiles = {};
+
+  for (final String link in googleFontsLinks) {
+    fontFiles.addAll(await getFileLinksFromGoogleFontsLink(link));
+  }
+
+  for (final fontFileDef in fontFiles) {
+    final Uri uri = Uri.parse(fontFileDef.url);
+
+    final Directory fontDir = Directory(p.join(fontsDir.path, fontFileDef.family))..createSync();
+    final String filename = "${fontFileDef.family}_${fontFileDef.weight}_${fontFileDef.style}${p.extension(fontFileDef.url)}";
+
+    final File fontFile = File(p.join(fontDir.path, filename));
+    final Uint8List bytes = await client.readBytes(uri);
+    await fontFile.writeAsBytes(bytes);
+  }
+
+  client.close();
+
+  print(fontFiles.join("\n"));
+}
+
+Future<List<FontFileDefinition>> getFileLinksFromGoogleFontsLink(String link) async {
+  final RegExp familyExtractor = RegExp(r"font-family:\s*(.*);", caseSensitive: false);
+  final RegExp styleExtractor = RegExp(r"font-style:\s*(.*);", caseSensitive: false);
+  final RegExp weightExtractor = RegExp(r"font-weight:\s*(.*);", caseSensitive: false);
+  final RegExp urlExtractor = RegExp(r"url\((.*?)\)", caseSensitive: false);
+
+  final Uri uri = Uri.parse(link);
+  final String cssFile = await client.read(uri);
+  final fontFaces = cssFile.split("@font-face").where((str) => str.trim().isNotEmpty);
+
+  final List<FontFileDefinition> results = [];
+  for (final String fontFace in fontFaces) {
+    final String family = familyExtractor.firstMatch(fontFace)!.group(1)!.trimQuotes();
+    final String style = styleExtractor.firstMatch(fontFace)!.group(1)!;
+    final String weight = weightExtractor.firstMatch(fontFace)!.group(1)!;
+    final String url = urlExtractor.firstMatch(fontFace)!.group(1)!;
+    results.add((family: family, style: style, weight: weight, url: url));
+  }
+
+  return results;
+}
+
+extension StringExtensions on String {
+  String trimQuotes() {
+    final RegExp unQuoter = RegExp(r"""^(["'])(.*)\1$""", multiLine: true);
+    return unQuoter.firstMatch(this)!.group(2)!;
+  }
+}
