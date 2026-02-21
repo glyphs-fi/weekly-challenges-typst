026da905ee4c0bb286c3b3b237e9d5824408d80f
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-02-21 02:18:29 +0100

    Clean up potential version numbers from dir names

--- fonts_downloader/downloaders/archive.dart
+++ fonts_downloader/downloaders/archive.dart
@@ -16,10 +16,47 @@ Future<void> downloadArchivedFonts({
     final File archiveFile = File(p.join(fontsDir.path, p.basename(link)));
     await downloadFile(client, url: link, destinationPath: archiveFile.path);
 
+    String? nDirname;
     // Extract the archive file into the fonts directory
-    await extractFileToDisk(archiveFile.path, fontsDir.path);
+    await extractFileToDisk(
+      archiveFile.path,
+      fontsDir.path,
+      callback: (ArchiveFile entry) {
+        // Record the shortest name; that will be the name of the directory that the files will be extracted to
+        if (nDirname == null || entry.name.length < nDirname!.length) {
+          nDirname = entry.name;
+        }
+      },
+    );
 
     // Delete the archive file
     await archiveFile.delete();
+
+    // If a name was recorded, we clean up any potential version numbers from it
+    if (nDirname != null) {
+      final String dirname = nDirname!;
+
+      // The directory that we just extracted from the archive
+      final Directory extractedDir = Directory(p.join(fontsDir.path, dirname));
+      // Remove version number from the extracted dir's name
+      final String cleanedUpPath = extractedDir.path.replaceFirst(RegExp(r"-[\d.]*?/?$"), "");
+      final Directory dirWithCleanedUpName = Directory(cleanedUpPath);
+
+      // If the downloader has already run before
+      if (dirWithCleanedUpName.existsSync()) {
+        // So we move any potential .gitignore file into the newly extracted directory
+        final File gitignoreFile = File(p.join(cleanedUpPath, ".gitignore"));
+        if (gitignoreFile.existsSync()) {
+          await gitignoreFile.rename(p.join(extractedDir.path, ".gitignore"));
+        }
+
+        // Then we delete old directory
+        await dirWithCleanedUpName.delete(recursive: true);
+        // And we replace it with the newly extracted one
+      }
+
+      // Rename the extracted directory with the cleaned up name
+      await extractedDir.rename(cleanedUpPath);
+    }
   }
 }
